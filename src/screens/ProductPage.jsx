b44efcc8db83a504370cd0f3ac6b64d41e7970fc
Nelson <david.saber44@gmail.com>
2024-03-23 10:19:39 -0500

    added go to top when select product

--- src/screens/ProductPage.jsx
+++ src/screens/ProductPage.jsx
@@ -12,10 +12,13 @@ function ProductPage({id}) {
       const response = await fetch('https://api.escuelajs.co/api/v1/products/' + id);
       const data = await response.json();
       setProduct(data);
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     };
-
+    
     fetchProduct();
+    
   }, [id]);
+  
   
 
   return (
